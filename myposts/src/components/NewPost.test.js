0b78392332040d05dc09388b49343b163e26c7ef
Ryan Sutcliffe <ryansutc@gmail.com>
2019-08-11 20:15:47 -0700

    updated test w. snapshot

--- myposts/src/components/NewPost.test.js
+++ myposts/src/components/NewPost.test.js
@@ -1,9 +1,10 @@
 import '@testing-library/react/cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
-import {render, fireEvent, Queries} from '@testing-library/react';
+import { render, fireEvent, Queries } from '@testing-library/react';
 
 import React from 'react';
 import NewPost from './NewPost';
+import Post from './Post';
 import CreatePost from '../containers/CreatePost';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
@@ -11,25 +12,12 @@ import App from './App';
 
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
-
-
-// normally here you'd do:
-// const div = document.createElement('div');
-//     const store = createStore(
-//         rootReducer,
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//         );
-        
-//         ReactDOM.render(
-//             <Provider store={store}>
-//                 <CreatePost />
-//             </Provider>, div);
-// but for this test we'll umm... not do that :)
+import renderer from 'react-test-renderer';
 
 // Now here's what your test will look like:
 
 function renderWithRedux(ui,
-  {initialState, store = createStore(rootReducer, initialState)} = {},
+  { initialState, store = createStore(rootReducer, initialState) } = {},
 ) {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
@@ -41,19 +29,29 @@ function renderWithRedux(ui,
 }
 
 test('show that a new post was created on create Post click', () => {
-    const {getByLabelText, getByPlaceholderText, getByText, container} = renderWithRedux(
-        <App />,
-    )
-        //okay, we want to add title and body to the NewPost form, can 
-        // we just update state? Or we gotta mock filling form?
-        
-        let message = 'hey there, this is the body';
-        //see: https://testing-library.com/docs/guide-which-query
-        fireEvent.change(getByPlaceholderText(/title/i), {target: {value: 'chuck norris'}})
-        fireEvent.change(getByPlaceholderText(/body/i), {target: {value: message}})
-            
-        fireEvent.click(getByText("Add Post"))
-        
-        //ReactDOM.unmountComponentAtNode(div);
-        expect(getByText(message)).toBeInTheDocument();
+  const { getByLabelText, getByPlaceholderText, getByText, container } = renderWithRedux(
+    <App />,
+  )
+  //okay, we want to add title and body to the NewPost form, can 
+  // we just update state? Or we gotta mock filling form?
+
+  let message = 'hey there, this is the body';
+  //see: https://testing-library.com/docs/guide-which-query
+  fireEvent.change(getByPlaceholderText(/title/i), { target: { value: 'chuck norris' } })
+  fireEvent.change(getByPlaceholderText(/body/i), { target: { value: message } })
+
+  fireEvent.click(getByText("Add Post"))
+
+  //ReactDOM.unmountComponentAtNode(div);
+  expect(getByText(message)).toBeInTheDocument();
+})
+let props = { id: 5, title: "hey you", message: "this is the message" };
+it('renders correctly. Snapshot test', () => {
+  const tree = renderer
+    .create(
+      <Post
+        post={props}
+      > </Post>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 })
