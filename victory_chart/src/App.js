e746940f4ebd48037e036b21dce321375bd8785b
Ryan Sutcliffe <rsutcliffe@3vGeomatics.com>
2019-09-19 18:44:38 -0700

    trying to change tooltip FAIL

--- victory_chart/src/App.js
+++ victory_chart/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
 
 import {LineSegment, VictoryChart, VictoryTooltip, VictoryAxis, VictoryStack, VictoryLabel, 
-  VictoryLine, VictoryScatter, VictoryTheme, VictoryVoronoiContainer}  from 'victory';
+  VictoryLine, VictoryScatter, VictoryTheme, VictoryVoronoiContainer, VictoryGroup}  from 'victory';
 import * as data from './MockData';
+
+import ChartToolTip from "./ChartToolTip";
+import CustomFlyOut from './CustomFlyOut';
 
 function formatDateForLabel(dateString) {
   let d = new Date(0);
@@ -16,62 +19,111 @@ function formatDateForLabel(dateString) {
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '40vh' }}>
+    <div style={{width: '100vw', height: '100vh'}}>
+      
       <VictoryChart
+        animate={false} //NEED to be able to set font size
+        width={800} height={300}
         containerComponent={
           <VictoryVoronoiContainer
-            text="what!!"
-            labels={ d => `y: ${ d.disp_los }`}
             labelComponent={
               <VictoryTooltip
-                cornerRadius={3}
-                flyoutStyle={{ 
-                  fill: "white",
-                  strokeWidth: 1
-                }}
+                cornerRadius={0}
+                centerOffset={{x:2,y:2}}                 
               />
             }
           />
         }
-        width={900}
-        height={400}
-        theme={VictoryTheme.material}
-        
-        animate={true}
       >
-       <VictoryAxis
-       
-        animate={{
-          duration: 200,
-          easing: "bounce"
-        }}
-        axisComponent={<LineSegment type={"axis"}/>}
-        dependentAxis={true}
-       />
-        <VictoryStack
-            colorScale={"warm"}
-        >
-          <VictoryLine
-            data={data.data}
-            x={"acquisition_time"}
-            y={"disp_los"}
-            labelComponent={<VictoryLabel renderInPortal dy={-20}/>}
-            maxDomain={{ y: 0.8}}
-            minDomain={{ y: -0.8}}
-          />
+        <VictoryAxis name="Y access"
+          axisComponent={<LineSegment type={"axis"}/>}
+          dependentAxis={true} //this means it is y axis?
+          label="Displacement"
+          standalone={false}
+          // TODO need to add ticks for axis
+          // TODO need to add properties to force labelling of min/max domain vals
+        />
+        <VictoryAxis name="X access"
+          label="X Axis"
+          //tickFormat={(x) => formatDateForLabel(x)}
+          tickLabelComponent={<CustomizedAxisTick />}
+          standalone={false}
+          axisValue={-0.8} // TODO: make either min of data or range value variable
+          // TODO need to add ticks for axis
+        />
 
-          <VictoryScatter
-            data={data.data}
-            x={"acquisition_time"}
-            y={"disp_los"}
-            maxDomain={{ y: 0.8}}
-            minDomain={{ y: -0.8}}
-          />
+        <VictoryLine name="ZeroLine"
+          domain={{x: [1209459281, 1232272484]}} // TODO needs to dynamically be first and last value
+          y={() => 0} // TODO make dynamic to mid val
+
+          style={{
+            data: {stroke: "yellow", strokeWidth: 1.2},
+          }}
+        />  
+
+        <VictoryStack
+          domain={{x: [1209459281, 1232272484], y: [-0.8, 0.8]}} // TODO make dynamic
+        >
+          <VictoryGroup
+            
+            labels= {({ datum}) => datum._y}
+            labelComponent={
+              <VictoryTooltip
+                flyoutComponent={<CustomFlyOut payloadItems={data.data}/>}
+                style={{ fontSize: 10 }}
+              />
+
+              //<ChartToolTip
+              //  startDate={data.data[0].acquisition_time}
+              //  payload={[data]}
+              //  classes={{tooltip:"tooltip-456"}}
+              ///>
+            }
+          >
+
+            <VictoryLine
+              animate={true}
+              data={data.data}
+              x="acquisition_time"
+              y="disp_los"
+              interpolation="linear"      
+            />
+
+            <VictoryScatter
+              size={({ active }) => active ? 5 : 3}
+              data={data.data}
+              x="acquisition_time"
+              y="disp_los"
+              style={{ data: { fill: "green"} }}
+            />
+          </VictoryGroup>
         </VictoryStack>
       </VictoryChart>
 
     </div>
   );
+}
+
+
+class CustomizedAxisTick extends PureComponent {
+  //https://formidable.com/open-source/victory/docs/victory-axis#ticklabelcomponent
+  render() {
+    const { x, y, datum } = this.props;
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text
+          x={0}
+          y={0}
+          dy={16}
+          textAnchor="end"
+          //fill={axisTickColor}
+          transform="rotate(-35)"
+        >
+          {formatDateForLabel(datum)}
+        </text>
+      </g>
+    );
+  }
 }
 
 export default App;
---
+++ victory_chart/src/CustomFlyOut.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import moment from 'moment';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
+
+import { withStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+const styles = theme => ({
+  tooltip: {
+    padding: 15,
+    width: 180
+  }
+});
+
+const colors = [
+  "#2464a8",
+  "#ffba44",
+  "#a82445",
+  "#00c7f9",
+  "#9500f9",
+  "#00f932"
+];
+
+function formatDateForLabel(dateString) {
+  let d = new Date(0);
+  d.setUTCSeconds(dateString);
+  return d.toLocaleDateString();
+}
+
+class CustomFlyout extends React.Component {
+
+  formatDateRange(startDate, endDate) {
+    return moment(startDate).diff(endDate, "days");
+  }
+
+  render() {
+    const {x, y, orientation, datum, payloadItems} = this.props;
+    const newY = orientation === "bottom" ? y - 35 : y + 35;
+    return (
+      <Paper>
+      <g>
+        <circle cx={x} cy={newY} r="20" stroke="tomato" fill="none"/>
+        <circle cx={x} cy={newY} r="25" stroke="orange" fill="none"/>
+        <circle cx={x} cy={newY} r="30" stroke="gold" fill="none"/>
+        <Chip
+          size='small'
+          color="default"
+          label={"hahahahaha"}
+          style={{ backgroundColor: colors[0], width: "100%" }}
+        />
+      </g>
+      </Paper>
+      /*
+      <Paper>
+          <Grid container>
+            <Grid item>
+              <Typography paragraph>
+                {`At ${formatDateForLabel(datum._x)} \n
+                ( Over ${this.formatDateRange(formatDateForLabel(datum._x), this.props.startDate, "days")} days):`
+                }
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              spacing={8}
+              alignItems="stretch"
+              justify="space-evenly"
+            >
+              {payloadItems.map((pl, index) => {
+                return (
+                  <Grid item key={index}>
+                    <Chip
+                      size='small'
+                      color="primary"
+                      label={Math.round(pl.value * 100) / 100 + " " + pl.unit}
+                      style={{ backgroundColor: colors[index], width: "100%" }}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Grid>
+        </Paper>
+      */
+    );
+  }
+}
+
+export default withStyles(styles)(CustomFlyout);
