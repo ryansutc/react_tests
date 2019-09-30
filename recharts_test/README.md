e796b6b28cf1a035dc680fbb07fc27a541dc892c
Ryan Sutcliffe <rsutcliffe@3vGeomatics.com>
2019-09-30 16:04:02 -0700

    added recharts sandbox

---
+++ recharts_test/README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+https://v3.material-ui.com/layout/grid/
+
---
+++ recharts_test/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Chart from './Chart';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Test 123 </p>
+      </header>
+
+      <Chart />
+     
+    </div>
+  );
+}
+
+export default App;
